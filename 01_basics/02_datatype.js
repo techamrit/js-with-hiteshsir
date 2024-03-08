a7d8e1a5802249b612dcec3e16d4cc9a7c87a940
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-08 09:39:30 +0000

    Add datat type names

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version.
+
+// alert(3 - 3 ) we are using node js not browser.
+console.log(
+    3
+    +
+    3
+)//code readabilaty should be high
+//below 10 line code written way is wrong don't do this
+console.log(amrit); console.log(2-2)// code readabilaty should be high
+
+let name="amrit";
+let age = 23;
+let inloggedin = false;
+let state;
+
+//number=> 2 to power 53;
+//bignit
+//strintg => ""
+//boolean => true/false;
+//null => standealone value;
+//undefined
+//symbol => unique;
+
+//object;
+
+console.log(typeof undefined)// undefined
+console.log(typeof null)// object this is interview question
