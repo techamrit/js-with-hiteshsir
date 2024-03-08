7846ed12f19f0674b92e6421469585e15702bbac
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-08 08:48:39 +0000

    add variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=12345;
+let accountEmail="amritp@gmail.com";
+var accountPassword = "23456";
+accountCity = "ghaziabad";
+
+// accountId=1234; not allowed
+accountEmail="amritpg@gmail.com";
+accountPassword="amrit";
+accountCity="mohan nagar";
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+
+*/ 
+
+
+
