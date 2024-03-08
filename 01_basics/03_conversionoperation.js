02ef4104e10d765da4c5993805e0eeb022b5d7e9
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-08 10:26:36 +0000

    add conversinoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+"use strict" //treat all code as a newer version.
+
+let score = 33;
+let scores = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof scores);
+
+let valueInNumber = Number(scores);//for garaunty is number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33"=> 33
+// 33abc => NaN;
+// true => 1 , false => 0;
+
+
+let loggedIn = "amrit";
+
+// console.log(typeof loggedIn)
+// console.log(loggedIn);
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+//0 => false 1 => true , (All true except 0);
+// "" typeof id string and boolean is false;
+//"amrit" is true
+
+
+//number  to string
+
+let someNo = 33;
+console.log(someNo);
+console.log(typeof someNo)
+
+let stringNumber = String(someNo)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
