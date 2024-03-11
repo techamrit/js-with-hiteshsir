274b3707e8328803ecda8fdc3cc631953a8a1f91
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-11 15:45:34 +0000

    how date workes

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,26 @@
+//date 
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let creatMyDate = new Date(2023, 0, 23)
+// let creatMyDate = new Date(2023, 0, 23,5,3)
+// let creatMyDate = new Date("2023-01-12")
+let creatMyDate = new Date("05-15-2023")//(month-day-year)
+// console.log(creatMyDate.getTime())
+
+let myTimeStmp = Date.now()
+// console.log(myTimeStmp)
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(newDate.getDay())
+// console.log(newDate.getMonth()+1)
+newDate.toLocaleString('default',{
+    weekday:"long",
+})
