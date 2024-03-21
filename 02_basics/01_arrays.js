e77c7bf33be3574925a0d79aa7f2d2cb118a1f04
Amrit Pal <amritpg662@gmail.com>
2024-03-21 22:51:22 +0530

    practic

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,32 +1,31 @@
-//array 
-const myArr = [0,1,2,3,4,5]
-const friends = ["aman", "kapil", "raj"]
+//array
+const myArr = [0, 1, 2, 3, 4, 5];
+const friends = ["aman", "kapil", "raj"];
 
-const myArr2 = new Array(1,2,3,4,5)
+const myArr2 = new Array(1, 2, 3, 4, 5);
 
 // console.table(myArr[0])
 
 //Array methods
 
-myArr.push(6)//add 6 in after 5
+myArr.push(6); //add 6 in after 5
 // console.log(myArr)
-myArr.push(7)//add 7 in after 6
-// console.log(myArr)
-
-myArr.pop()//remove 7 in array
+myArr.push(7); //add 7 in after 6
 // console.log(myArr)
 
+myArr.pop(); //remove 7 in array
+// console.log(myArr)
 
-myArr.unshift(9)//add 9 in start before 0
+myArr.unshift(9); //add 9 in start before 0
 // console.log(myArr)
 // myArr.shift()
-myArr.shift()//remove 9 from starting
+myArr.shift(); //remove 9 from starting
 // console.log(myArr)
 
 // console.log(myArr.includes(5))//retur value in boolean form (true/false)
 // console.log(myArr.indexOf(9))//if true so print value or (-1)
 
-const newArr = myArr.join()//convert in string
+const newArr = myArr.join(); //convert in string
 
 // console.log(myArr)
 // console.log(newArr)
@@ -35,14 +34,13 @@ const newArr = myArr.join()//convert in string
 //slice, splice
 
 // console.log("A", myArr);
-const myNewArr1 = myArr.slice(0,5)
+const myNewArr1 = myArr.slice(0, 5); //stack
 // console.log(myNewArr1)
 // console.log("B", myArr);
 
+const myNewArr2 = myArr.splice(0, 5); //heap
 
-const myNewArr2 = myArr.splice(0,5);
-
-console.log("c",myArr);
+console.log("c", myArr);
 
 console.log(myNewArr2);
 
