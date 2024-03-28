add646c06be3b28ce9bb050b2684bc6dfffde806
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-28 18:26:28 +0000

    check Object

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -51,7 +51,7 @@ const users = [
 // console.log(users[1]["email"]);
 
 // console.log(tinderUser);
-// console.log(Object.keys(tinderUser))
+// console.log(Object.keys(tinderUser)) 
 // console.log(Object.values(tinderUser))
 // console.log(Object.entries(tinderUser))
 
