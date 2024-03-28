2a0f4721217a77da3b4857b63d795253f7c4f939
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-28 18:26:49 +0000

    if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+const isUserLoggedIn = true;
+const temprature = 41;
+
+// if (temprature<=50) {
+//     console.log("less them 50");
+    
+// }else{
+//     console.log("more then 50 ")
+// }
+// console.log(temprature);//is line ka if statment sa koi lana dana nahi hai
+
+
+// <, >, <=, >=, ==, !=, ===, !==//compair element
+
+// const score = 200;
+
+// if (score >100) {
+//     const power = "fly"
+//     console.log(`user power is: ${power}`)
+// }
+
+
+const balance = 1000;
+
+// if(balance>500) console.log("yes");
+// if(balance>5000) console.log("yes"),console.log("this is wrong way for write");
+
+// if (balance<500) {
+//     console.log("less than 500");
+// }
+// else if (balance<700) {
+//     console.log("less than 700");
+// } else if (balance<900) {
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less then 1100");
+// }
+
+
+const loggedIn = true;
+const debitCard = true;
+const loggedInGoogle = false;
+const loggedInEmail = true;
+
+// if (loggedIn && debitCard) {
+//    console.log("allow to logged in");
+    
+// }
+
+if (loggedInGoogle|| loggedInEmail) {
+    console.log("allow to logged in 2");
+}
