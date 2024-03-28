0400a44c32bb40a03ea1fb9f3c3ce557dc55212b
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-03-28 18:27:14 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,98 @@
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+// }*/
+// const month=6;
+// switch (month) {
+//     case 1:console.log("January")
+        
+//        break;
+//     case 2:console.log("February")
+        
+//         break;
+//     case 3:console.log("March")
+        
+//      break;
+//     case 4:console.log("April")
+        
+//     break;
+//     case 5:console.log("May")
+        
+//         break;
+//     case 6:console.log("June")
+        
+//      break;
+//     case 7:console.log("july")
+        
+//         break;
+//     case 8:console.log("August")
+        
+//         break;
+//     case 9:console.log("September")
+        
+//         break;
+//     case 10:console.log("October")
+        
+//         break;
+//     case 11:console.log("November")
+        
+//         break;
+//     case 12:
+//         console.log("December")
+        
+//         break;
+
+//     default:
+//         console.log(`${month}th   month is not avilable. 12 month only in a year` );
+//         break;
+// }
+
+
+const month="dec";
+switch (month) {
+    case "jan":console.log("January")
+        
+       break;
+    case"feb":console.log("February")
+        
+        break;
+    case "mar":console.log("March")
+        
+     break;
+    case "apr":console.log("April")
+        
+    break;
+    case "may":console.log("May")
+        
+        break;
+    case "jun":console.log("June")
+        
+     break;
+    case "jul":console.log("july")
+        
+        break;
+    case "aug":console.log("August")
+        
+        break;
+    case "sep":console.log("September")
+        
+        break;
+    case "oct":console.log("October")
+        
+        break;
+    case "nov":console.log("November")
+        
+        break;
+    case "dec":
+        console.log("December")
+        
+        break;
+
+    default:
+        console.log(`${month}th   month is not avilable. 12 month only in a year` );
+        break;
+}
