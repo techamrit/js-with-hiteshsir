85b062675eaff5f888a683fa6dfc98df7bbd8dbe
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-04-01 12:19:52 +0000

    continoue and break

--- 05_iterations/for.js
+++ 05_iterations/for.js
@@ -26,7 +26,31 @@ for (let i = 0; i <=2; i++) {
         const element = j;
         // console.log(`inner loop value: ${j}`);
 
-        console.log(`${i} * ${j} = ${i*j}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
         
     }
+}
+let myArray = ["amrit","manish","kakku"];
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element)
+}
+
+//break 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(i==6){
+        // console.log(`ditect 6`)
+        break;
+    }
+    // console.log(`value of i is ${i}`);
+}
+//and contioue
+for (let index = 0; index <=20; index++) {
+    const element = index;
+    if (index==10) {
+        console.log(`five skip`);   
+    }
+    console.log(`value of ${index}is skip`);
 }
