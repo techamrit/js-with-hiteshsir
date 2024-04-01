5759a1cfb6f354c9f70aacb97f531915cc2e3c11
Amrit Pal <160051195+techamrit@users.noreply.github.com>
2024-04-01 16:56:27 +0000

    while and do-while

---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,22 @@
+//while
+
+// let i = 1
+// while (i<=10) {
+//     // console.log(`value of ${i}`);
+//     i=i+1
+// }
+
+//array
+// let myArray = ["amrit","manish","kakku"];
+
+// let i=0;
+// while (i<myArray.length) {
+//     const myArr= myArray[i]
+//     i++
+//     console.log(myArr);
+// }
+let score = 1
+do {
+    console.log(`score ${score}`);
+    score++
+} while (score<=15);
